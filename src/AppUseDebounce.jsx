8b8a9650d160a6b34770e05fc15bb70dabddd916
ishant.oberoi <ishant.oberoi@nutanix.com>
2020-09-23 22:09:04 +0530

    more comments

--- src/AppUseDebounce.jsx
+++ src/AppUseDebounce.jsx
@@ -11,20 +11,20 @@ import _ from "lodash";
 // React Hooks must be called in a React function component or a custom React Hook function
 
 // // OPTION 1: DEBOUNCE FUNCTION
-// const AppUseDebounce = () => {
-//   const [val, setVal] = useState(0);
-//   const [dVal, setDVal] = useState(0); // debounced value
-//   const [state, setState] = useState({ data: null, loading: true });
+const AppUseDebounce = () => {
+  const [val, setVal] = useState(0);
+  const [dVal, setDVal] = useState(0); // debounced value
+  const [state, setState] = useState({ data: null, loading: true });
 
 
 //   /* TRICK:
 //   How debounce works?
-//   We create a function once using debounce and if that function is called
+//   We create a function once using call to debounce() and if that function is called
 //   again within the within the wait time, a new timeout is initiated.
 //   So it remembers the last timeout value along with other private variables.
 //   In functional react components, if we just create the debounce function myFn(below)
 //   , it will be re-created fresh on every render. Hence if you directly call
-//   it in handleChange myFn(e.target.value) it will be fired for all changing values.
+//   it in handleChange myFn(e.target.value) it will be fired for all changing input values.
 
 //   So idea is to stop re-creation of function. So use hook useCallback.
 //   This now memoizes the debounce function. Same fun is used for next render.
@@ -46,107 +46,93 @@ import _ from "lodash";
 //   const fn = _.debounce(...) and calling it fn(). Though the function is
 //   memoized with useCallback but when it fires, inside we create a new fn everytime
 //   using debounce. So wrong implementation.  */
-//   // Option: 2
-  // const makeCall = useCallback((newVal) => {
-  //   const fn = _.debounce((newVal) => {
-  //     console.log('Hi')
-  //     setState({ data: null, loading: true })
-  //     fetch(`http://numbersapi.com/${newVal}`)
-  //       .then(x => x.text())
-  //       .then(y => {
-  //         setState({ data: y, loading: false });
-  //         setDVal(newVal);
-  //       });
-  //   }, 2000);
-  //   fn(newVal);
-  // }, []);
+// Option: 2
+// const makeCall = useCallback((newVal) => {
+//   const fn = _.debounce((newVal) => {
+//     console.log('Hi')
+//     setState({ data: null, loading: true })
+//     fetch(`http://numbersapi.com/${newVal}`)
+//       .then(x => x.text())
+//       .then(y => {
+//         setState({ data: y, loading: false });
+//         setDVal(newVal);
+//       });
+//   }, 2000);
+//   fn(newVal);
+// }, []);
 
-//   // Option: 3
-//    Create the myfn function once and useCallback to memoize the
-//   result of it _.debounce() result. ie memoize myfn. Here we are
-//   using this same myFn definition on re-render.
+//   Option: 3
+//   Create the myfn function EVERYTIME and useCallback to memoize the
+//   result of it _.debounce() result. ie memoize a anonymous function
+//   that calls return of myfn() essentially return of debounce().
+//   So fn2 reference is cached inside this anonymous function
+//   after the initial render.
+//   Here we are
+//   using this same fn2 definition on re-render.
+//   Check 'outside debounce' is logged on everytime but
+//   'inside debounce' is logged only after debounce timeout.
 
-//   // const myfn = _.debounce((newVal) => {
-//   //   console.log('Hi');
-//   //   setState({ data: null, loading: true })
-//   //   fetch(`http://numbersapi.com/${newVal}`)
-//   //     .then(x => x.text())
-//   //     .then(y => {
-//   //       setState({ data: y, loading: false });
-//   //       setDVal(newVal);
-//   //     });
-//   // }, 2000);
-//   // const makeCall = useCallback((newVal) => myfn(newVal), []);
+// const myfn = () => {
+//   console.log('outside debounce');
+//   return _.debounce((newVal) => {
+//     console.log('inside debounce');
+//     setState({ data: null, loading: true });
+//     fetch(`http://numbersapi.com/${newVal}`)
+//       .then(x => x.text())
+//       .then(y => {
+//         setState({ data: y, loading: false });
+//         setDVal(newVal);
+//       });
+//   }, 2000);
+// };
 
-//   // Option: 4
-//   /* pass the debounce as reference to useCallback.
+// const fn2 = myfn();
+
+// const makeCall = useCallback((newVal) => { fn2(newVal);}, []);
+
+// Option: 4
+// TRICK: the return function from debounce should not be created
+// everytime. It should be memoized so that it remembers it timeout and other private
+// variables.
+// const myFn = _.debounce((newVal) => {
+//   console.log('inside debounce');
+//   setState({ data: null, loading: true });
+//   fetch(`http://numbersapi.com/${newVal}`)
+//     .then(x => x.text())
+//     .then(y => {
+//       setState({ data: y, loading: false });
+//       setDVal(newVal);
+//     });
+// }, 2000);
+// const makeCall = useCallback((newVal) => myFn(newVal), []);
+
+//   Option: 5
+//   /* directly CALL debounce inside useCallback and memoize the return
+//   function from debounce.
 //   note: useCallback(() => {}, []) we are using debounce instead of anonymous
 //   callback function. useCallback(debounce(), []).
-//   So this entire definition is memoized and not re-created.
+//   So debounce is called and its return function is replacing the usual
+//   anonynous function (() => {}) inside the useCallback. And this return
+//   function definition is memoized.
 //   Same concept as the definition with anonymous function is memoized
 //   by useCallback. So this part _.debounce((newVal)...) is created once
 //   and the returned function is used everytime same as myfn from Option 3.
 //   As the dependencies are [] */
 
-//   // NOTE: If you change the dependencies of useCallback to [someVar] or omit
-//   // the empty array altogether(useCallback(() => {})), you are back to re-creating
-//   // it every time.
-//   // Now same behaviour as Option 1 and 2.
-  // const makeCall = useCallback(_.debounce((newVal) => {
-  //   console.log('Hi');
-  //   setState({ data: null, loading: true });
-  //   fetch(`http://numbersapi.com/${newVal}`)
-  //     .then(x => x.text())
-  //     .then(y => {
-  //       setState({ data: y, loading: false });
-  //       setDVal(newVal);
-  //     });
-  // }, 2000), []);
-
-//   return (
-//     <div>
-//       <input
-//         type="number"
-//         placeholder="enter number"
-//         value={ val }
-//         onChange={ e => {
-//           setVal(e.target.value);
-//           makeCall(e.target.value);
-//         }} />
-//       <div>
-//         { state.loading && 'loading...' }
-//         { !state.loading && state.data }
-//       </div>
-//       <div>{ dVal }</div>
-//     </div>
-//   );
-// }
-
-
-
-// OPTION 2: DEBOUNCE VALUE
-// Trick: delay setting a state value or prop.
-// Then you can use it to fire api or perform action
-// after the delay
-const useDebounceVal = ({ value, delay }) => {
-  const [dVal, setDVal] = useState(value);
-
-  const deb = useCallback(_.debounce((value) => {
-    console.log('value in callback', value);
-    setDVal(value);
-  }, delay), []);
-
-  useEffect(() => {
-    deb(value);
-  });
-
-  return dVal;
-}
-
-const AppUseDebounce = () => {
-  const [val, setVal] = useState(0);
-  const debounceVal = useDebounceVal({ value: val, delay: 2000 });
-  const { data, loading } = useFetch(`http://numbersapi.com/${debounceVal}`);
+  // NOTE: If you change the dependencies of useCallback to [someVar] or omit
+  // the empty array altogether(useCallback(() => {})), you are back to re-creating
+  // it every time.
+  // Now same behaviour as Option 1 and 2.
+  const makeCall = useCallback(_.debounce((newVal) => {
+    console.log('Hi');
+    setState({ data: null, loading: true });
+    fetch(`http://numbersapi.com/${newVal}`)
+      .then(x => x.text())
+      .then(y => {
+        setState({ data: y, loading: false });
+        setDVal(newVal);
+      });
+  }, 2000), []);
 
   return (
     <div>
@@ -156,15 +142,60 @@ const AppUseDebounce = () => {
         value={ val }
         onChange={ e => {
           setVal(e.target.value);
+          makeCall(e.target.value);
         }} />
       <div>
-        { loading && 'loading...' }
-        { !loading && data }
+        { state.loading && 'loading...' }
+        { !state.loading && state.data }
       </div>
-      <div>{ debounceVal }</div>
+      <div>{ dVal }</div>
     </div>
   );
 }
+
+
+
+// OPTION 2: DEBOUNCE VALUE
+// Trick: delay setting a state value or prop.
+// Then you can use it to fire api or perform action
+// after the delay
+// const useDebounceVal = ({ value, delay }) => {
+//   const [dVal, setDVal] = useState(value);
+
+//   const deb = useCallback(_.debounce((value) => {
+//     console.log('value in callback', value);
+//     setDVal(value);
+//   }, delay), []);
+
+//   useEffect(() => {
+//     deb(value);
+//   });
+
+//   return dVal;
+// }
+
+// const AppUseDebounce = () => {
+//   const [val, setVal] = useState(0);
+//   const debounceVal = useDebounceVal({ value: val, delay: 2000 });
+//   const { data, loading } = useFetch(`http://numbersapi.com/${debounceVal}`);
+
+//   return (
+//     <div>
+//       <input
+//         type="number"
+//         placeholder="enter number"
+//         value={ val }
+//         onChange={ e => {
+//           setVal(e.target.value);
+//         }} />
+//       <div>
+//         { loading && 'loading...' }
+//         { !loading && data }
+//       </div>
+//       <div>{ debounceVal }</div>
+//     </div>
+//   );
+// }
 
 // Class Component Debounce: EASY
 // class AppUseDebounce extends React.Component {
